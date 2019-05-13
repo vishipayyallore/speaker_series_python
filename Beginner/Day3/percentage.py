d595de6f37b4bcac2bfbd693177b286bfe045c6e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-13 21:37:26 +0530

    Code Refactoring

--- Beginner/Day3/percentage.py
+++ Beginner/Day3/percentage.py
@@ -1,17 +1,25 @@
 import math
 
-n = int(input())
-student_marks = {}
+def find_percentages():
+    n = int(input())
+    student_marks = {}
 
-for _ in range(n):
-    name, *line = input().split()
-    scores = list(map(float, line))
-    student_marks[name] = scores
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+        
+    query_name = input()
+
+    current_marks = student_marks[query_name]
+    average = (sum(current_marks) / len(current_marks)) * 1.00
+    print("{0:.2f}".format(average))
+
+
+def main():
+    find_percentages()
+
+
+if __name__ == "__main__":
+    main()
     
-query_name = input()
-
-current_marks = student_marks[query_name]
-average = (sum(current_marks) / len(current_marks)) * 1.00
-print("{0:.2f}".format(average))
-
-
