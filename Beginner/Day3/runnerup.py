b9bae7c58fe4d090c11f6e03abba914c2eedef0f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-13 21:41:20 +0530

    Code Refactoring

--- Beginner/Day3/runnerup.py
+++ Beginner/Day3/runnerup.py
@@ -1,11 +1,20 @@
 
-n = int(input())
-numbers = input()
+def find_runnerup():
+    n = int(input())
+    numbers = input()
 
-numbers_list = list(map(int, numbers.split()))
-numbers_list = list(dict.fromkeys(sorted(numbers_list)))
-print(numbers_list)
-print(numbers_list[-2])
+    numbers_list = list(map(int, numbers.split()))
+    numbers_list = list(dict.fromkeys(sorted(numbers_list)))
+    print(numbers_list)
+    print(numbers_list[-2])
+
+
+def main():
+    find_runnerup()
+
+
+if __name__ == "__main__":
+    main()
 
 # numbers_list = list(map(int, numbers.split()))
 # numbers_list = [int(i) for i in numbers.split()] 
--- Beginner/Day3/sample.py
+++ Beginner/Day3/sample.py
@@ -1,15 +1,25 @@
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
 
-# with filter
-output = [[i, j, k] for i in range(
-    x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)]
-print(output)
+def create_matrix():
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
 
-# without filter
-output = [[i, j, k] for i in range(x + 1)
-          for j in range(y + 1) for k in range(z + 1)]
-for item in output:
-    print(item)
+    # with filter
+    output = [[i, j, k] for i in range(
+        x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)]
+    print(output)
+
+    # without filter
+    output = [[i, j, k] for i in range(x + 1)
+            for j in range(y + 1) for k in range(z + 1)]
+    for item in output:
+        print(item)
+
+
+def main():
+    create_matrix()
+
+
+if __name__ == "__main__":
+    main()
