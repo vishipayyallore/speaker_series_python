b7473d9d0dac3e92647d64c6ea92a267b95c85b6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-21 22:34:32 +0530

    ..

--- Logicals/May2019/21/listdemo.py
+++ Logicals/May2019/21/listdemo.py
@@ -1,18 +1,14 @@
 
-numbers = []
-
-
-def perform_action(data):
-    if(data[0] == 'insert'):
-        numbers.insert(int(data[1]), int(data[2]))
-    elif(data[0] == 'insert'):
-        numbers.append(int(data[1]))
-    elif(data[0] == 'remove'):
-        numbers.remove(int(data[1]))
 
 
 if __name__ == '__main__':
+    numbers = []
+
     N = int(input())
 
     for counter in range(N):
-        data = input()
+        command, *arguments = input().split()
+        if(command != 'print'):
+            eval(f'numbers.{command}{tuple(map(int, arguments))}')
+        else:
+            print(numbers) 
