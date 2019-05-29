4126bf188c6eec327dffd86a653c194231a85358
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-29 21:03:59 +0530

    Small UPdate

--- Logicals/May2019/29/textwrap.py
+++ Logicals/May2019/29/textwrap.py
@@ -2,8 +2,9 @@ import textwrap
 
 
 def wrap(string, max_width):
-    wrapper = textwrap.TextWrapper(width=4)
-    return "\n".join(wrapper.wrap(text=string))
+    # wrapper = textwrap.TextWrapper(width=4)
+    # return "\n".join(wrapper.wrap(text=string))
+    return '\n'.join(textwrap.wrap(string, max_width))
 
 
 if __name__ == '__main__':
