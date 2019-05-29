eb4aa30bb9cb65c2a36daa160bea9b901b903614
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-29 20:26:26 +0530

    small program

---
+++ Logicals/May2019/29/textwrap.py
@@ -0,0 +1,10 @@
+import textwrap
+
+def wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width=4) 
+    return "\n".join(wrapper.wrap(text=string))
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
