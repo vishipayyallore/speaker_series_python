2723a8971536a416b8e9889c6f6491d6fa333f07
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-31 21:17:55 +0530

    2nd version of the logic

--- Logicals/May2019/29/textwrap.py
+++ Logicals/May2019/29/textwrap.py
@@ -2,16 +2,23 @@ import textwrap
 
 
 def wrap(string, max_width):
-    # wrapper = textwrap.TextWrapper(width=4)
-    # return "\n".join(wrapper.wrap(text=string))
     return '\n'.join(textwrap.wrap(string, max_width))
+
+
+def wrap_v2(string, max_width):
+    list_data = textwrap.wrap(string, max_width)
+    return '\n'.join(list_data)
 
 
 def main():
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
     print(result)
-    
+
+    result = wrap_v2(string, max_width)
+    print(result)
+
 
 if __name__ == '__main__':
     main()
+
