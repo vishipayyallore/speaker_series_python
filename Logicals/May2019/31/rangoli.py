746e3018d4123b24cd4468d396896adea3fa0565
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-01 20:50:17 +0530

    ..

--- Logicals/May2019/31/rangoli.py
+++ Logicals/May2019/31/rangoli.py
@@ -10,5 +10,14 @@ if __name__ == '__main__':
     # print_rangoli(n)
     n = 5
     current_data = alphabets[0:n]
-    print(current_data)
+    print(current_data, current_data[::-1])
     print('-'.join(alphabets[n-1]))
+
+#size 3
+"""
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+"""
