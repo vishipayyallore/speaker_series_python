6add37b0131689f653553e854ab8098083ffad4a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-04 22:50:51 +0530

    ..

--- Logicals/Jun2019/01/testchars.py
+++ Logicals/Jun2019/01/testchars.py
@@ -11,7 +11,13 @@ if __name__ == '__main__':
     value = "".join(list(filter(str.isupper, s)))
     print(value.isupper())
 
-    
+    # Version 2
+    print (any(c.isalnum()  for c in s))
+    print (any(c.isalpha() for c in s))
+    print (any(c.isdigit() for c in s))
+    print (any(c.islower() for c in s))
+    print (any(c.isupper() for c in s))
+
 
     """
     
