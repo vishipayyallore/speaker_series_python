c49ac38dd61307c9c62cb96297a7122b0dd9be38
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-04 22:50:37 +0530

    Simple program

--- Logicals/Jun2019/01/testchars.py
+++ Logicals/Jun2019/01/testchars.py
@@ -2,7 +2,21 @@ if __name__ == '__main__':
     s = input()
 
     print(s.isalnum())
-    print(s.isalpha())
+    value = "".join(list(filter(str.isalpha, s)))
+    print(value.isalpha())
+    value = "".join(list(filter(str.isdigit, s)))
+    print(value.isdigit())
+    value = "".join(list(filter(str.islower, s)))
+    print(value.islower())
+    value = "".join(list(filter(str.isupper, s)))
+    print(value.isupper())
+
+    
+
+    """
+    
+    
     print(s.isdigit())
     print(s.islower())
     print(s.isupper())
+    """
