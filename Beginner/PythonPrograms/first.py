f2d4c55f3ab3ff8e70775ac1d367e9cd0aec8ab1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-07-07 00:16:15 +0530

    First Program

---
+++ Beginner/PythonPrograms/first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+    Program to demonstrate Basics of Input/Output
+"""
+
+# Importing Modules
+import datetime
+
+# Calculating the date
+todays_date = datetime.datetime.now()
+today = todays_date.strftime('%c')
+
+# Accepting Inputs from User
+name = input("Enter Your Name: ")
+employee_id = int(input("Enter Employee Id: "))
+salary = float(input("Enter Salary: "))
+is_manager = bool(input("Enter Manager Status: "))
+
+# Displaying the output
+print("====================================================")
+print(f"Employee Details | Date: {today} |")
+print("====================================================")
+print(f"\tName: {name}")
+print(f"\tId: {employee_id}")
+print(f"\tSalary: {salary}")
+print(f"\tIs Manager: {is_manager}")
+print("----------------------------------------------------")
